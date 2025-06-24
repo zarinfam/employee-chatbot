bb19df4d66f9ad83cdc38c640b597a9201ef7eca
Saeed Zarinfam <zarinfam.s@gmail.com>
2025-06-24 08:50:18 +0200

    Upgrade to Spring AI 1.0

--- src/main/java/com/saeed/employee/chatbot/config/ChatbotConfig.java
+++ src/main/java/com/saeed/employee/chatbot/config/ChatbotConfig.java
@@ -1,9 +1,10 @@
 package com.saeed.employee.chatbot.config;
 
-import org.springframework.ai.chat.client.advisor.RetrievalAugmentationAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -15,7 +16,10 @@ public class ChatbotConfig {
 
     @Bean
     ChatMemory chatMemory() {
-        return new InMemoryChatMemory();
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(new InMemoryChatMemoryRepository())
+                .maxMessages(50) 
+                .build();
     }
 
     @Bean
